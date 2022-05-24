34fc4ef51884a93f98b7934d85c0a4d1b7b3f934
Mark Roldan <mark.roldan@cardihab.com>
2022-05-25 07:48:33 +0800

    Add listener to definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,4 +1,10 @@
+import { PluginListenerHandle } from "@capacitor/core";
+
 export interface AppleWatchConnectivityPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
   sendMessage(options: { messageId: string; message: Array<any> }): Promise<void>;
+  addListener(
+    eventName: 'taskSelect',
+    listenerFunc: (data: any) => void,
+  ): Promise<PluginListenerHandle> & PluginListenerHandle;
 }
